d3f6b28aa14d7dde9dabbd77ffa34bb2991f618b
Daniel Godin <danielgodin@Daniels-MacBook-Air.local>
2022-12-04 07:06:54 -0800

    add, subtract, multiply, and divide functions created

---
+++ script.js
@@ -0,0 +1,17 @@
+// Create 4 functions. add, subtract, multiply, divide (Start with 2 parameters).
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}

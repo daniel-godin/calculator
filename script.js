26b0e3f547ed7504fd228ccab8fb809617cbc1b4
Daniel Godin <danielgodin@Daniels-MacBook-Air.local>
2022-12-04 07:11:49 -0800

    function operate created.  Takes 3 parameters (operator, num1, num2) and passes them along to first 4 functions depending on first parameter.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Create 4 functions. add, subtract, multiply, divide (Start with 2 parameters).
-
 function add(a, b) {
     return a + b;
 }
@@ -14,4 +13,10 @@ function multiply(a, b) {
 
 function divide (a, b) {
     return a / b;
+}
+
+// Create a function that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+
+function operate(op, c, d) {
+    return op(c, d); 
 }

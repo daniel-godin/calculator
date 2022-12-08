cbdcb33b982aeefa878aa28187e0bf93aea3a9a1
Daniel Godin <danielgodin@Daniels-MacBook-Air.local>
2022-12-08 09:16:26 -0800

    Cleaned up lots of extra space.  Rewrote some comments.  Eliminated the unneeded variable creation to match event.key in the keyboard event listener.

--- script.js
+++ script.js
@@ -19,42 +19,28 @@ let secondNumber;
 let operator;
 let answer;
 
-
-// Adding Keyboard support to calculator.  I wonder if I can send both keyboard and click event listeners to a function with all the rest?  Hmmm, let's try.
+// Keyboard support for calculator.  Checks the event.key and triggers a button click.  Uses a for loop for all buttons except backspace and enter and clear.
 window.addEventListener('keydown', (event) => {
-    let keyName = event.key;
-    // loop through button array, if keyName === button[i].value do something.
-
-    if (keyName === "Backspace") {
+    if (event.key === "Backspace") {
         event.preventDefault();
         backspaceButton.click();
-        } else if (keyName === "Enter") {
+    } else if (event.key === "Enter") {
         event.preventDefault();
         equalsButton.click();
-        } else {
-
-            for (let i = 0; i < buttons.length; i++) {
-            // if (keyName === "Backspace") {
-            //     event.preventDefault();
-            //     backspaceButton.click();
-            // } else if (keyName === "Enter") {
-            //     event.preventDefault();
-            //     equalsButton.click();
-            // } else 
-                if (keyName === buttons[i].value) {
-
-                    event.preventDefault();
-                    buttons[i].click();
-            
-                }
-            }    
-        }     
+    } else {
+        for (let i = 0; i < buttons.length; i++) {
+            if (event.key === buttons[i].value) {
+                event.preventDefault();
+                buttons[i].click();
+            }
+        }    
+    }     
 })
 
-// Function which changes display to match the value of the number button clicked.  Stores value in variable numValue.
+// Function which triggers on button clicks.  Stores that value.  Activates operate function when appropriate.  It also updates the display text.
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        if (button.classList.contains('number-button')) {
+        if (button.classList.contains('number-button')) { // Number Buttons.
             if (firstNumber != undefined) {
                 numArray.push(button.value);
                 mainDisplay.innerText = `${firstNumber} ${operator} ${numArray.join("")}`
@@ -62,9 +48,7 @@ buttons.forEach((button) => {
                 numArray.push(button.value);
                 mainDisplay.innerText = numArray.join("");
             }
-        }
-
-        if (button.classList.contains('decimal-button')) {
+        } else if (button.classList.contains('decimal-button')) { // Decimal Button.
             if (firstNumber != undefined) {
                     if (numArray.includes(".")) {
                         mainDisplay.innerText = `${firstNumber} ${operator} ${numArray.join("")}`
@@ -72,7 +56,6 @@ buttons.forEach((button) => {
                         numArray.push(button.value);
                         mainDisplay.innerText = `${firstNumber} ${operator} ${numArray.join("")}`
                     }
-
             } else if (firstNumber == undefined) {
                 if (numArray.includes(".")) {
                     mainDisplay.innerText = numArray.join("");
@@ -81,8 +64,7 @@ buttons.forEach((button) => {
                     mainDisplay.innerText = numArray.join("");
                 }
             }
-
-        } else if (button.classList.contains('operator-button')) {
+        } else if (button.classList.contains('operator-button')) { // Operator Buttons.
             if (numArray.length < 1 && firstNumber == undefined) {
                 mainDisplay.innerText = "";
             } else if (numArray.length > 0 && firstNumber != undefined) { // Uses this if the operate() function has been run.  Thus, user is running a calculation on the answer, AND has already input some numbers.
@@ -109,7 +91,6 @@ buttons.forEach((button) => {
                     firstNumber = parseFloat(answer);
                     secondNumber = undefined;
                 }
-               
             } else if (answer != undefined && numArray.length < 1) { // function operate() has been run and the answer is now the firstNumber.  User has not input any numbers yet for a secondNumber variable.
                 operator = button.value;
                 mainDisplay.innerText = `${firstNumber} ${operator}`;
@@ -123,10 +104,9 @@ buttons.forEach((button) => {
                 numArray.length = 0;
             }
 
-        } else if (button.classList.contains('equal-button')) {
+        } else if (button.classList.contains('equal-button')) { // Equal Button.
             if (firstNumber == undefined && operator == undefined) {
                 mainDisplay.innerText = `${numArray.join("")}`;
-
             } else if (firstNumber != undefined && operator != undefined && numArray.length > 0) {
                 secondNumber = numArray.join("");
                 secondNumber = parseFloat(secondNumber);
@@ -151,22 +131,16 @@ buttons.forEach((button) => {
                 }
             }
 
-        } else if (button.classList.contains('clear-button')) {
+        } else if (button.classList.contains('clear-button')) { // Clear Button.
             clear();
-
-        } else if (button.classList.contains('backspace-button')) {
-            console.log(mainDisplay.innerText.length);
+        } else if (button.classList.contains('backspace-button')) { // Backspace Button.
             if (mainDisplay.innerText.length === 1) {
                 clear();
             } else {
                 // save mainDisplay.innerText in a variable, then remove the last number in the string, then recreate the mainDisplay from that.
                 mainDisplay.innerText = mainDisplay.innerText.substring(0, mainDisplay.innerText.length - 1);
-                console.log(mainDisplay.innerText);
                 numArray.pop();
             }
-
-
-
         }
     })
 })
